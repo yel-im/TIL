64a0e88d25b0d6e71d7c554675f135f07a36b036
yelim <choyr2016@naver.com>
2021-08-20 23:56:57 +0900

    .

--- react/pagination/src/components/Pagination.js
+++ react/pagination/src/components/Pagination.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Pagination = ( { postsPerPage, totalPosts, paginate } ) => {
     const pageNumbers = [];
     for(let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+        // Math.ceil => 주어진 숫자보다 크거나 같은 숫자 중 가장 작은 숫자를 정수형으로 반환함
         pageNumbers.push(i);
     }
     return (
